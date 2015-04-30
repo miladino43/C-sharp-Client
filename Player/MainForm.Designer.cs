646e213d4f2558477aee102b77add36c424344c5
Milad Bonakdar <mba67@sfu.ca>
2015-04-29 23:09:38 -0700

    c# client code
    client server application for aerial wireless security camera

---
+++ Player/MainForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Player
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.fpsLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_connection_status = new System.Windows.Forms.Label();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.right_button = new System.Windows.Forms.Button();
+            this.left_button = new System.Windows.Forms.Button();
+            this.up_button = new System.Windows.Forms.Button();
+            this.down_button = new System.Windows.Forms.Button();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.mainMenuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(529, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fpsLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 335);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(529, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // fpsLabel
+            // 
+            this.fpsLabel.Name = "fpsLabel";
+            this.fpsLabel.Size = new System.Drawing.Size(514, 17);
+            this.fpsLabel.Spring = true;
+            this.fpsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.label2);
+            this.mainPanel.Controls.Add(this.lbl_connection_status);
+            this.mainPanel.Controls.Add(this.connect_button);
+            this.mainPanel.Controls.Add(this.right_button);
+            this.mainPanel.Controls.Add(this.left_button);
+            this.mainPanel.Controls.Add(this.up_button);
+            this.mainPanel.Controls.Add(this.down_button);
+            this.mainPanel.Controls.Add(this.videoSourcePlayer);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 24);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(529, 311);
+            this.mainPanel.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label2";
+            // 
+            // lbl_connection_status
+            // 
+            this.lbl_connection_status.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.lbl_connection_status.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_connection_status.Location = new System.Drawing.Point(46, 10);
+            this.lbl_connection_status.MaximumSize = new System.Drawing.Size(100, 100);
+            this.lbl_connection_status.Name = "lbl_connection_status";
+            this.lbl_connection_status.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbl_connection_status.Size = new System.Drawing.Size(100, 13);
+            this.lbl_connection_status.TabIndex = 10;
+            // 
+            // connect_button
+            // 
+            this.connect_button.Location = new System.Drawing.Point(410, 47);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(75, 23);
+            this.connect_button.TabIndex = 8;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // right_button
+            // 
+            this.right_button.Location = new System.Drawing.Point(460, 138);
+            this.right_button.Name = "right_button";
+            this.right_button.Size = new System.Drawing.Size(32, 23);
+            this.right_button.TabIndex = 7;
+            this.right_button.Text = ">";
+            this.right_button.UseVisualStyleBackColor = true;
+            this.right_button.Click += new System.EventHandler(this.right_button_Click);
+            // 
+            // left_button
+            // 
+            this.left_button.Location = new System.Drawing.Point(404, 138);
+            this.left_button.Name = "left_button";
+            this.left_button.Size = new System.Drawing.Size(32, 23);
+            this.left_button.TabIndex = 6;
+            this.left_button.Text = "<";
+            this.left_button.UseVisualStyleBackColor = true;
+            this.left_button.Click += new System.EventHandler(this.left_button_Click);
+            // 
+            // up_button
+            // 
+            this.up_button.Location = new System.Drawing.Point(432, 109);
+            this.up_button.Name = "up_button";
+            this.up_button.Size = new System.Drawing.Size(32, 23);
+            this.up_button.TabIndex = 5;
+            this.up_button.Text = "^";
+            this.up_button.UseVisualStyleBackColor = true;
+            this.up_button.Click += new System.EventHandler(this.up_button_Click);
+            // 
+            // down_button
+            // 
+            this.down_button.Location = new System.Drawing.Point(432, 167);
+            this.down_button.Name = "down_button";
+            this.down_button.Size = new System.Drawing.Size(32, 23);
+            this.down_button.TabIndex = 4;
+            this.down_button.Text = "v";
+            this.down_button.UseVisualStyleBackColor = true;
+            this.down_button.Click += new System.EventHandler(this.down_button_Click);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.videoSourcePlayer.ForeColor = System.Drawing.Color.White;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(46, 35);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.videoSourcePlayer.Size = new System.Drawing.Size(322, 242);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "AVI files (*.avi)|*.avi|All files (*.*)|*.*";
+            this.openFileDialog.Title = "Opem movie";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(529, 357);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.mainMenuStrip);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Simple Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripStatusLabel fpsLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button right_button;
+        private System.Windows.Forms.Button left_button;
+        private System.Windows.Forms.Button up_button;
+        private System.Windows.Forms.Button down_button;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.Label lbl_connection_status;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Player/MainForm.cs
@@ -0,0 +1,229 @@
+﻿// Simple Player sample application
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2006-2011
+// contacts@aforgenet.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Sockets;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Player
+{
+    public partial class MainForm : Form
+    {
+        //Int32 port = 23;
+        TcpClient client = new TcpClient();
+        private Stopwatch stopWatch = null;
+        private int start = Environment.TickCount;
+
+
+        // Class constructor
+        // Initialized form and starts video stream
+        public MainForm()
+        {
+            InitializeComponent();
+ //           label1.Text = charCount.ToString();
+       
+            // create video source
+            MJPEGStream mjpegSource = new MJPEGStream("http://192.168.0.115:99/videostream.cgi?user=admin&pwd=");
+            // open it
+            OpenVideoSource(mjpegSource);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCurrentVideoSource();
+        }
+
+        // "Exit" menu item clicked
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Capture 1st display in the system
+        private void capture1stDisplayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenVideoSource(new ScreenCaptureStream(Screen.AllScreens[0].Bounds, 100));
+        }
+
+        // Open video source
+        private void OpenVideoSource(IVideoSource source)
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // stop current video source
+            CloseCurrentVideoSource();
+
+            // start new video source
+            videoSourcePlayer.VideoSource = source;
+            videoSourcePlayer.Start();
+
+            // reset stop watch
+            stopWatch = null;
+
+            // start timer
+            timer.Start();
+
+            this.Cursor = Cursors.Default;
+        }
+
+        // Close video source if it is running
+        private void CloseCurrentVideoSource()
+        {
+            if (videoSourcePlayer.VideoSource != null)
+            {
+                videoSourcePlayer.SignalToStop();
+
+                // wait ~ 3 seconds
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!videoSourcePlayer.IsRunning)
+                        break;
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                if (videoSourcePlayer.IsRunning)
+                {
+                    videoSourcePlayer.Stop();
+                }
+
+                videoSourcePlayer.VideoSource = null;
+            }
+        }
+
+        // New frame received by the player
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+           
+            DateTime now = DateTime.Now;
+            Graphics g = Graphics.FromImage(image);
+
+            // paint current time
+            SolidBrush brush = new SolidBrush(Color.Red);
+            g.DrawString(now.ToString(), this.Font, brush, new PointF(5, 5));
+            brush.Dispose();
+
+            g.Dispose();
+        }
+
+        // On timer event - gather statistics
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            IVideoSource videoSource = videoSourcePlayer.VideoSource;
+
+            if (videoSource != null)
+            {
+                // get number of frames since the last timer tick
+                int framesReceived = videoSource.FramesReceived;
+
+                if (stopWatch == null)
+                {
+                    stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                }
+                else
+                {
+                    stopWatch.Stop();
+
+                    float fps = 1000.0f * framesReceived / stopWatch.ElapsedMilliseconds;
+                    fpsLabel.Text = fps.ToString("F2") + " fps";
+
+                    stopWatch.Reset();
+                    stopWatch.Start();
+                }
+            }
+        }
+
+        // Connect to wifi shield s
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            // Create a TcpClient.
+            // Note, for this client to work you need to have a TcpServer 
+            // connected to the same address as specified by the server, port
+            // combination.
+
+            client.Connect("192.168.0.101", 23);
+            lbl_connection_status.Text = "Client Socket Program - Server Connected ...";
+
+        }
+
+        private void up_button_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Up";
+            byte[] buf = System.Text.Encoding.ASCII.GetBytes("U");
+            client.GetStream().Write(buf, 0, buf.Length);
+        }
+
+        private void right_button_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Right";
+            byte[] buf = System.Text.Encoding.ASCII.GetBytes("R");
+            client.GetStream().Write(buf, 0, buf.Length);
+        }
+
+        private void down_button_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Down";
+            byte[] buf = System.Text.Encoding.ASCII.GetBytes("D");
+            client.GetStream().Write(buf, 0, buf.Length);
+        }
+
+        private void left_button_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Left";
+            byte[] buf = System.Text.Encoding.ASCII.GetBytes("L");
+            client.GetStream().Write(buf, 0, buf.Length);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+
+            byte[] buf = System.Text.Encoding.ASCII.GetBytes("None");
+            if ( (Environment.TickCount - start) > 300) 
+            {
+                switch (keyData)
+                {
+                    case Keys.Up:
+                        start = Environment.TickCount;
+                        label2.Text = "Up";
+                        buf = System.Text.Encoding.ASCII.GetBytes("U");
+                        client.GetStream().Write(buf, 0, buf.Length);
+                        break;
+                    case Keys.Down:
+                        start = Environment.TickCount;
+                        label2.Text = "Down";
+                        buf = System.Text.Encoding.ASCII.GetBytes("D");
+                        client.GetStream().Write(buf, 0, buf.Length);
+                        break;
+                    case Keys.Left:
+                        start = Environment.TickCount;
+                        label2.Text = "Left";
+                        buf = System.Text.Encoding.ASCII.GetBytes("L");
+                        client.GetStream().Write(buf, 0, buf.Length);
+                        break;
+                    case Keys.Right:
+                        start = Environment.TickCount;
+                        label2.Text = "Right";
+                        buf = System.Text.Encoding.ASCII.GetBytes("R");
+                        client.GetStream().Write(buf, 0, buf.Length);
+                        break;
+                }
+            }
+            return true;
+        }
+   
+    }
+}
